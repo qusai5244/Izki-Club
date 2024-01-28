e54f3d6b0df3770e5aabca23f99fccf8bbb18424
qusay alnaabi <qalnaabi00@gmail.com>
2024-01-28 10:20:15 -0800

    add Referee Contorller

---
+++ Izki-Club/Services/Interfaces/IRefereeService.cs
@@ -0,0 +1,16 @@
+﻿using Izki_Club.Dtos.GeneralDtos;
+using Izki_Club.Dtos.MemberDtos;
+using Izki_Club.Dtos.PlayerDtos;
+using Izki_Club.Dtos.RefereeDtos;
+
+namespace Izki_Club.Services.Interfaces
+{
+    public interface IRefereeService
+    {
+        Task<ApiResponse<ViewRefereeDto>> CreatReferee(AddRefereeDto input);
+        Task<ApiResponse<ViewRefereeDto>> DeleteReferee(int id);
+        Task<ApiResponse<ViewRefereeDto>> GetReferee(int Id);
+        Task<ApiResponse<PaginatedList<ViewRefereeDto>>> GetReferees(SearchAndPaginationDto input);
+        Task<ApiResponse<ViewRefereeDto>> UpdateReferee(int id, UpdateRefereeDto input);
+    }
+}
